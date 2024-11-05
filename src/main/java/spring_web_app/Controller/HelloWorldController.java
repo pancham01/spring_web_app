7c39b045e7a2dde05aa55cfe732534f35097945a
pancham01 <pancham9557@gmail.com>
2024-11-05 15:10:46 +0530

    update

--- src/main/java/spring_web_app/Controller/HelloWorldController.java
+++ src/main/java/spring_web_app/Controller/HelloWorldController.java
@@ -7,12 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
