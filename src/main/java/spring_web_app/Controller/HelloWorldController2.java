60bfd588f06c840f84f3d46ec905acbdc017aec4
pancham01 <pancham9557@gmail.com>
2025-04-13 10:49:53 +0530

    update

--- src/main/java/spring_web_app/Controller/HelloWorldController2.java
+++ src/main/java/spring_web_app/Controller/HelloWorldController2.java
@@ -20,81 +20,41 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/footwear")
 public class HelloWorldController2 {
 
-//	 @RequestMapping(method = RequestMethod.GET)
 	@GetMapping(path = { "/getmapping", "/fetchMapping" })
 	public String get(HttpServletRequest req, HttpServletResponse res) {
-		
 		System.out.println("Footwear - Controller- fetchMapping");
 		return "admin";
 	}
 
-	// @GetMapping(path= {"/getmapping"}, params="name")
-	// public String getParam(HttpServletRequest req, HttpServletResponse res) {
 	@GetMapping(path = { "/getParam" })
 	public String getParam(@RequestParam(name = "name", defaultValue = "Guest") String name) {
 		System.out.println("Controller-  params=name: " + name);
 		return "index";
 	}
 
-	// hello/getCustomer/1234/details
 	@ResponseBody
 	@GetMapping(path = { "/getCustomer/{customerId}/details" })
 	public String getPathVariable(@PathVariable String customerId) {
-
 		System.out.println("Controller-  params=customerId : " + customerId);
 		return "Controller-  params=customerId : " + customerId;
 	}
 
-	
-	
-	
-//	@RequestMapping("/setCookie")
-//	public String setCookie(HttpServletResponse response) {
-//	    Cookie cookie = new Cookie("myCookie", "DangerValue");
-//	    cookie.setMaxAge(3600); // Cookie will expire in 1 hour
-//	    response.addCookie(cookie);
-//	    return "redirect:/hello/second";
-//	}
-	
-	
-	
-	
-	
+	@RequestMapping("/setCookie")
+	public String setCookie(HttpServletResponse response) {
+		Cookie cookie = new Cookie("myCookie", "DangerValue");
+		cookie.setMaxAge(1110); // Cookie will expire in 1 hour
+		response.addCookie(cookie);
+		System.out.println("HelloWorldController2.setCookie()");
+		return "redirect:/footwear/second";
+	}
+
 	@RequestMapping("/second")
-    public String secondPage(@CookieValue(value = "myCookie", defaultValue = "defaultCookieValue1") String myCookieValue, Model model) {
-        // Use myCookieValue in your controller logic
-        model.addAttribute("cookiesValue", myCookieValue);
-        return "second";
-    }
-	
-	
-	@GetMapping(path = { "/getCookie" })
-	public String getCookiesValue(HttpServletRequest req, @CookieValue("JSESSIONID") String jsessionCookie) {
-		String value= "";
-		Cookie[] cookies = req.getCookies();
-		for (Cookie cookie : cookies) {
-			if (cookie.getName().equals("JSESSIONID")) {
-				value = cookie.getValue();
-			}
-		}
-		System.out.println("Controller-  cookie=" + value);
-		System.out.println("Controller-  jsessionCookie=" + jsessionCookie);
-		return "index";
+	public String secondPage(
+			@CookieValue(value = "myCookie", defaultValue = "defaultCookieValue1") String myCookieValue, Model model) {
+		// Use myCookieValue in your controller logic
+		System.out.println("HelloWorldController2.secondPage()");
+		model.addAttribute("cookiesValue", myCookieValue);
+		return "second";
 	}
 
-	// @RequestMapping(method = RequestMethod.POST)
-	@PostMapping
-	public String post(HttpServletRequest req, HttpServletResponse res) {
-
-		System.out.println("Controller- POST");
-		return "index";
-	}
-
-	// @RequestMapping(method = RequestMethod.DELETE)
-	@DeleteMapping
-	public String delete(HttpServletRequest req, HttpServletResponse res) {
-
-		System.out.println("Controller- DELETE");
-		return "index";
-	}
 }
